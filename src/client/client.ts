d34c95a739c81fe2a681d7e587baeb4807e1caa4
Landon Dyken <ldyken53@uab.edu>
2022-08-10 14:38:26 -0500

    Initial working map viewer with triplanar mapping

---
+++ src/client/client.ts
@@ -0,0 +1,115 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
+import { terrainShader } from "./shaders/terrain-shader"
+import Stats from 'three/examples/jsm/libs/stats.module'
+
+const scene = new THREE.Scene()
+
+// const light = new THREE.SpotLight()
+// light.position.set(4000, 4000, 20)
+// scene.add(light)
+// const ambient = new THREE.AmbientLight( 0x404040 ); // soft white light
+// scene.add( ambient );
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    5000
+)
+camera.position.set(2000, 1000, 1000);
+
+const renderer = new THREE.WebGLRenderer()
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target = new THREE.Vector3(2000,1000,-1000);
+
+const satelliteTexture = new THREE.TextureLoader().load('./satellite.png');
+// scene.background = satelliteTexture;
+
+const uvTexture = new THREE.TextureLoader().load('./img/grid.png');
+// scene.background = uvTexture;
+
+const material = new THREE.MeshStandardMaterial({
+    map: uvTexture,
+});
+
+const boxG = new THREE.BoxGeometry(100, 100, 100);
+const boxM = new THREE.MeshStandardMaterial({
+    map: uvTexture,
+});
+const box = new THREE.Mesh(boxG, boxM);
+// scene.add(box);
+
+const satelliteLoader = new THREE.TextureLoader();
+satelliteLoader.load(
+    "./satellite.png",
+    function (texture) {
+      var uniforms1 = {
+        diffuseTexture: { type: "t", value: texture },
+      };
+      const meshMaterial = new THREE.RawShaderMaterial({
+        uniforms: uniforms1,
+        vertexShader: terrainShader._VS,
+        fragmentShader: terrainShader._FS,
+      });
+      const terrainLoader = new STLLoader()
+      terrainLoader.load(
+          'elev500.stl',
+          function (geometry) {
+              geometry.computeBoundingBox();
+              geometry.computeVertexNormals();
+
+              const mesh = new THREE.Mesh(geometry, meshMaterial);
+              mesh.receiveShadow = true;
+              mesh.castShadow = true;
+              scene.add(mesh)
+              mesh.position.set(0, 0, -100);
+              console.log(mesh);
+              console.log("test")
+          },
+          (xhr) => {
+              // console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+          },
+          (error) => {
+              console.log(error)
+          }
+      )
+    },
+    undefined,
+    function (err) {
+      console.error("An error happened.");
+    }
+  );
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    controls.update()
+
+    render()
+
+    stats.update()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
---
+++ src/client/shaders/terrain-shader.ts
@@ -0,0 +1,61 @@
+const _VS = `#version 300 es
+precision highp float;
+
+uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+in vec3 position;
+in vec3 normal;
+
+out vec3 vNormal;
+out vec3 vPosition;
+
+#define saturate(a) clamp( a, 0.0, 1.0 )
+
+void main(){
+    vNormal = normal;
+    vPosition = position.xyz / vec3(4104.0, 1856.0, 500.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    // vPosition = gl_Position.xyz*vec3(0.5,0.5,0.5) + vec3(0.5,0.5,0.5);
+  }
+`;
+
+const _FS = `#version 300 es
+
+precision mediump sampler2DArray;
+precision highp float;
+precision highp int;
+
+uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
+uniform vec3 cameraPosition;
+uniform sampler2D diffuseTexture;
+
+in vec3 vNormal;
+in vec3 vPosition;
+
+out vec4 out_FragColor;
+
+vec3 blendNormal(vec3 normal){
+	vec3 blending = abs(normal);
+	blending /= (blending.x + blending.y + blending.z);
+	return blending;
+}
+
+vec3 triplanarMapping (sampler2D tex, vec3 normal, vec3 position) {
+  vec3 normalBlend = blendNormal(normal);
+  vec3 xColor = texture(tex, position.yz).rgb;
+  vec3 yColor = texture(tex, position.xz).rgb;
+  vec3 zColor = texture(tex, position.xy).rgb;
+  return (xColor * normalBlend.x + yColor * normalBlend.y + zColor * normalBlend.z);
+}
+
+void main(){
+    vec3 color = triplanarMapping(diffuseTexture, vNormal, vPosition);
+    // vec3 color = texture(diffuseTexture, vPosition.xy * 1000);
+    // out_FragColor = texture(diffuseTexture, vPosition.yz);
+    out_FragColor = vec4(color, 1.0);
+  }
+`;
+export const terrainShader = { _VS, _FS };
