dbb4b158eb4277ff3c43482a5133b167edd67be7
Landon Dyken <ldyken53@uab.edu>
2022-10-26 07:28:48 -0500

    Fix region 10

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,14 +1,13 @@
-const terrainDimensions : {[key: string]: number[]}= {
+const terrainDimensions : {[key: string]: number[]} = {
     '1': [4104, 1856],
     '2': [6472, 3136],
-    '3': [10528, 4480],
     '4': [4032, 1792],
     '5': [4704, 2240],
     '6': [5500, 2700],
     '7': [5400, 2800],
     '8': [5100, 2800],
     '9': [5500, 3000],
-    '10': [5500, 2783],
+    '3': [5500, 2783],
 }
 export {
     terrainDimensions
